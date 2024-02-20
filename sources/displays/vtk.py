f2bfad7aa1fbbca5d965d4f9f912d8fd7f7f950e
KeplerDream <engue11@hotmail.com>
2024-02-20 20:39:00 +0100

    Adding Quaternion Handling
    - You now have the choice between euler angles and quaternions
    - You can register both quaternions and euler angles and they are saved in memory

--- sources/displays/vtk.py
+++ sources/displays/vtk.py
@@ -53,8 +53,12 @@ class VtkDisplay(BasicDisplay):
         if os.path.exists(self.meshFilePath):
             self.vtkMesh = pv.read(self.meshFilePath)
             self.vtkDisplay.add_mesh(self.vtkMesh)
-        self.rotation = {'SET_ROTATION': editWidget.rotationCheckbox.isChecked(), 'ROTATION_TYPE': 'EULER',
-                         'ARGUMENTS': [editWidget.rollEdit.text(), editWidget.pitchEdit.text(), editWidget.yawEdit.text()]}
+        self.rotation = {'SET_ROTATION': editWidget.rotationCheckbox.isChecked(),
+                         'ROTATION_TYPE': 'EULER' if editWidget.rotationTypeComboBox.currentIndex() == 0 else 'QUATERNION',
+                         'ARGUMENTS': [editWidget.rollEdit.text(), editWidget.pitchEdit.text(),
+                                       editWidget.yawEdit.text(), editWidget.qwEdit.text(),
+                                       editWidget.qxEdit.text(), editWidget.qyEdit.text(),
+                                       editWidget.qzEdit.text()]}
 
     def updateContent(self, content=None):
         self.generalSettings = loadSettings('settings')
@@ -76,25 +80,22 @@ class VtkDisplayEditDialog(QWidget):
     def __init__(self, path, parent: VtkDisplay):
         super().__init__(parent)
         self.settings = loadSettings('settings')
+        themeFolder = 'dark-theme' if self.settings['DARK_THEME'] else 'light-theme'
+        meshPixMap = QPixmap(f'sources/icons/{themeFolder}/icons8-file-explorer-96.png').scaled(25, 25)
+        selectionButtonPixmap = QPixmap(f'sources/icons/{themeFolder}/icons8-add-database-96.png').scaled(25, 25)
         self.currentDir = path
         self.hide()
 
         # MESH FILE EDITION
-        themeFolder = 'dark-theme' if self.settings['DARK_THEME'] else 'light-theme'
-        meshPixMap = QPixmap(f'sources/icons/{themeFolder}/icons8-file-explorer-96.png').scaled(25, 25)
         meshFileLabel = QLabel('Mesh file: ')
         self.meshFileEdit = QLineEdit(parent.meshFilePath)
         self.meshSelectionButton = QPushButton()
         self.meshSelectionButton.setIcon(QIcon(meshPixMap))
         self.meshSelectionButton.clicked.connect(self.selectMeshFile)
 
-        # MESH FILE ROTATION
-        self.rotationCheckbox = QCheckBox('Add Rotation')
-        self.rotationCheckbox.stateChanged.connect(self.toggleRotation)
-        self.rotationCheckbox.setChecked(parent.rotation['SET_ROTATION'])
-        self.rotationFrame = QFrame()
-        self.rotationFrame.setEnabled(parent.rotation['SET_ROTATION'])
-        selectionButtonPixmap = QPixmap(f'sources/icons/{themeFolder}/icons8-add-database-96.png').scaled(25, 25)
+        # EULER ANGLES FRAME
+        self.eulerFrame = QFrame()
+        self.eulerFrame.setEnabled(parent.rotation['SET_ROTATION'])
         rollLabel, pitchLabel, yawLabel = QLabel('Roll: '), QLabel('Pitch:'), QLabel('Yaw:  ')
         self.rollEdit = QLineEdit(parent.rotation['ARGUMENTS'][0])
         self.pitchEdit = QLineEdit(parent.rotation['ARGUMENTS'][1])
@@ -107,25 +108,72 @@ class VtkDisplayEditDialog(QWidget):
         self.pitchButton.clicked.connect(self.openArgumentSelectorPitch)
         self.yawButton.clicked.connect(self.openArgumentSelectorYaw)
 
-        frameLayout = QGridLayout(self.rotationFrame)
-        frameLayout.addWidget(rollLabel, 0, 0)
-        frameLayout.addWidget(pitchLabel, 1, 0)
-        frameLayout.addWidget(yawLabel, 2, 0)
-        frameLayout.addWidget(self.rollEdit, 0, 1)
-        frameLayout.addWidget(self.pitchEdit, 1, 1)
-        frameLayout.addWidget(self.yawEdit, 2, 1)
-        frameLayout.addWidget(self.rollButton, 0, 2)
-        frameLayout.addWidget(self.pitchButton, 1, 2)
-        frameLayout.addWidget(self.yawButton, 2, 2)
+        eulerFrameLayout = QGridLayout(self.eulerFrame)
+        eulerFrameLayout.addWidget(rollLabel, 0, 0)
+        eulerFrameLayout.addWidget(pitchLabel, 1, 0)
+        eulerFrameLayout.addWidget(yawLabel, 2, 0)
+        eulerFrameLayout.addWidget(self.rollEdit, 0, 1)
+        eulerFrameLayout.addWidget(self.pitchEdit, 1, 1)
+        eulerFrameLayout.addWidget(self.yawEdit, 2, 1)
+        eulerFrameLayout.addWidget(self.rollButton, 0, 2)
+        eulerFrameLayout.addWidget(self.pitchButton, 1, 2)
+        eulerFrameLayout.addWidget(self.yawButton, 2, 2)
+
+        # QUATERNION FRAME
+        self.quaternionFrame = QFrame()
+        self.eulerFrame.setEnabled(parent.rotation['SET_ROTATION'])
+        qwLabel, qxLabel, qyLabel, qzLabel = QLabel('W: '), QLabel('X: '), QLabel('Y: '), QLabel('Z: ')
+        self.qwEdit = QLineEdit(parent.rotation['ARGUMENTS'][3])
+        self.qxEdit = QLineEdit(parent.rotation['ARGUMENTS'][4])
+        self.qyEdit = QLineEdit(parent.rotation['ARGUMENTS'][5])
+        self.qzEdit = QLineEdit(parent.rotation['ARGUMENTS'][6])
+        self.qwButton, self.qxButton = QPushButton(), QPushButton()
+        self.qyButton, self.qzButton = QPushButton(), QPushButton()
+        self.qwButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qxButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qyButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qzButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qwButton.clicked.connect(self.openArgumentSelectorQw)
+        self.qxButton.clicked.connect(self.openArgumentSelectorQx)
+        self.qyButton.clicked.connect(self.openArgumentSelectorQy)
+        self.qzButton.clicked.connect(self.openArgumentSelectorQz)
+
+        quaternionFrameLayout = QGridLayout(self.quaternionFrame)
+        quaternionFrameLayout.addWidget(qwLabel, 0, 0)
+        quaternionFrameLayout.addWidget(qxLabel, 1, 0)
+        quaternionFrameLayout.addWidget(qyLabel, 2, 0)
+        quaternionFrameLayout.addWidget(qzLabel, 3, 0)
+        quaternionFrameLayout.addWidget(self.qwEdit, 0, 1)
+        quaternionFrameLayout.addWidget(self.qxEdit, 1, 1)
+        quaternionFrameLayout.addWidget(self.qyEdit, 2, 1)
+        quaternionFrameLayout.addWidget(self.qzEdit, 3, 1)
+        quaternionFrameLayout.addWidget(self.qwButton, 0, 2)
+        quaternionFrameLayout.addWidget(self.qxButton, 1, 2)
+        quaternionFrameLayout.addWidget(self.qyButton, 2, 2)
+        quaternionFrameLayout.addWidget(self.qzButton, 3, 2)
+
+        # MESH FILE ROTATION
+        self.rotationCheckbox = QCheckBox('Add Rotation')
+        self.rotationCheckbox.stateChanged.connect(self.toggleRotation)
+        self.rotationCheckbox.setChecked(parent.rotation['SET_ROTATION'])
+        self.rotationTypeComboBox = QComboBox()
+        self.rotationTypeComboBox.addItems(['Euler Angles', 'Quaternion'])
+        self.rotationTypeComboBox.setEnabled(parent.rotation['SET_ROTATION'])
+        self.rotationTypeComboBox.currentIndexChanged.connect(self.showRotationFrame)
 
         # MAIN LAYOUT
         mainLayout = QGridLayout(self)
         mainLayout.addWidget(meshFileLabel, 0, 0)
         mainLayout.addWidget(self.meshFileEdit, 0, 1)
         mainLayout.addWidget(self.meshSelectionButton, 0, 2)
-        mainLayout.addWidget(self.rotationCheckbox, 1, 0, 1, 3)
-        mainLayout.addWidget(self.rotationFrame, 2, 0, 1, 3)
-        self.setLayout(mainLayout)
+        mainLayout.addWidget(self.rotationCheckbox, 1, 0)
+        mainLayout.addWidget(self.rotationTypeComboBox, 1, 1)
+        mainLayout.addWidget(self.eulerFrame, 2, 0, 1, 3)
+        mainLayout.addWidget(self.quaternionFrame, 2, 0, 1, 3)
+
+        # SETTING RIGHT EDITION FRAME
+        self.rotationTypeComboBox.setCurrentText('Euler Angles' if parent.rotation['ROTATION_TYPE'] == 'EULER' else 'Quaternion')
+        self.showRotationFrame(0 if parent.rotation['ROTATION_TYPE'] == 'EULER' else 1)
 
     def openArgumentSelectorRoll(self):
         dialog = ArgumentSelector(self.currentDir, self)
@@ -148,8 +196,46 @@ class VtkDisplayEditDialog(QWidget):
             self.yawEdit.setText(dialog.selectedArgument)
             self.yawEdit.adjustSize()
 
+    def openArgumentSelectorQw(self):
+        dialog = ArgumentSelector(self.currentDir, self)
+        result = dialog.exec_()
+        if result == QMessageBox.Accepted:
+            self.qwEdit.setText(dialog.selectedArgument)
+            self.qwEdit.adjustSize()
+
+    def openArgumentSelectorQx(self):
+        dialog = ArgumentSelector(self.currentDir, self)
+        result = dialog.exec_()
+        if result == QMessageBox.Accepted:
+            self.qxEdit.setText(dialog.selectedArgument)
+            self.qxEdit.adjustSize()
+
+    def openArgumentSelectorQy(self):
+        dialog = ArgumentSelector(self.currentDir, self)
+        result = dialog.exec_()
+        if result == QMessageBox.Accepted:
+            self.qyEdit.setText(dialog.selectedArgument)
+            self.qyEdit.adjustSize()
+
+    def openArgumentSelectorQz(self):
+        dialog = ArgumentSelector(self.currentDir, self)
+        result = dialog.exec_()
+        if result == QMessageBox.Accepted:
+            self.qzEdit.setText(dialog.selectedArgument)
+            self.qzEdit.adjustSize()
+
+    def showRotationFrame(self, index):
+        if index == 0:  # Euler Angles
+            self.eulerFrame.setVisible(True)
+            self.quaternionFrame.setVisible(False)
+        elif index == 1:  # Quaternion
+            self.eulerFrame.setVisible(False)
+            self.quaternionFrame.setVisible(True)
+
     def toggleRotation(self, state):
-        self.rotationFrame.setEnabled(state)
+        self.eulerFrame.setEnabled(state)
+        self.quaternionFrame.setEnabled(state)
+        self.rotationTypeComboBox.setEnabled(state)
 
     def selectMeshFile(self):
         fileFilter = "VTK Files (*.vtk *.stl *.vtu *.vtp);;All Files (*)"
@@ -164,13 +250,16 @@ class VtkDisplayEditDialog(QWidget):
         self.rollButton.setIcon(QIcon(selectionButtonPixmap))
         self.pitchButton.setIcon(QIcon(selectionButtonPixmap))
         self.yawButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qwButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qxButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qyButton.setIcon(QIcon(selectionButtonPixmap))
+        self.qzButton.setIcon(QIcon(selectionButtonPixmap))
         self.meshSelectionButton.setIcon(QIcon(meshPixMap))
 
 
 class AxisButton(QPushButton):
     def __init__(self, parent=None):
         super().__init__(parent)
-        # Set the icon and icon size
         self.settings = loadSettings('settings')
         buttonTheme = 'dark-theme' if self.settings['DARK_THEME'] else 'light-theme'
         self.setIcon(QIcon(f'sources/icons/{buttonTheme}/icons8-axis-96.png'))
